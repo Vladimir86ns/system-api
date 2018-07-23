f99cda8279ffd018115a45e154c8a3d0ab396bb8
vladimir86ns <vladimir86ns@gmail.com>
2018-07-23 21:11:21 +0200

    feature/adding-investment-admin

---
+++ resources/views/chose_status.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Admin Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- global level css -->
+  <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+  <!-- end of global level css -->
+  <!-- page level css -->
+  <link href="{{ asset('assets/css/pages/login2.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/vendors/iCheck/css/minimal/blue.css') }}" rel="stylesheet"/>
+  <link href="{{ asset('assets/vendors/bootstrapvalidator/css/bootstrapValidator.min.css') }}" rel="stylesheet"/>
+  <!-- styles of the page ends-->
+</head>
+
+<body>
+  <div class="container">
+    <div class="row vertical-offset-100">
+      <div class=" col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3  col-md-5 col-md-offset-4 col-lg-4 col-lg-offset-4">
+
+        <div id="notific">
+          @include('notifications')
+        </div>
+
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title text-center">Chose Status</h3>
+          </div>
+
+          <div class="panel-body">
+            <a href="/employee/login" class="btn btn-primary btn-block btn-lg">Employee</a>
+            <a href="/owner/login" class="btn btn-primary btn-block btn-lg">Owner</a>
+            <a href="/investment/login" class="btn btn-primary btn-block btn-lg">Investor</a>
+            <a href="/investment-admin/login" class="btn btn-primary btn-block btn-lg">Investment Admin</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- global js -->
+    <script src="{{ asset('assets/js/app.js') }}" type="text/javascript"></script>
+    <!-- end of global js -->
+    <!-- begining of page level js-->
+    <script src="{{ asset('assets/js/TweenLite.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/iCheck/js/icheck.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/bootstrapvalidator/js/bootstrapValidator.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/js/pages/login2.js') }}" type="text/javascript"></script>
+    <!-- end of page level js-->
+  </body>
+  </html>
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,38 @@
 |
 */
 
-Route::get('/', 'AuthController@choseStatus')->name('chose-status');
+Route::get('/', 'AuthController@choseStatus');
+
+//  INVESTMENTS-ADMIN
+Route::group([ 'prefix' => 'investment-admin'], function () {
+    Route::get('/dashboard', 'InvestmentAdminController@dashboard');
+    // WITHOUT MIDDLEWARE
+    Route::get('/login', 'InvestmentAdminController@getSignIn');
+    Route::post('/sign-up', 'InvestmentAdminController@signUp');
+    Route::post('/sign-in', 'InvestmentAdminController@signIn');
+});
+
+//  INVESTOR
+Route::group([ 'prefix' => 'investment'], function () {
+
+    // WITHOUT MIDDLEWARE
+    Route::get('login', 'InvestmentController@getSignIn')->name('investment-login');
+});
+
+//  EMPLOYEE
+Route::group([ 'prefix' => 'employee'], function () {
+
+    // WITHOUT MIDDLEWARE
+    Route::get('login', 'EmployeeController@getSignIn')->name('employee-login');
+});
+
+//  COMPANY
+Route::group([ 'prefix' => 'owner'], function () {
+
+    // WITHOUT MIDDLEWARE
+    Route::get('login', 'OwnerController@getSignIn')->name('owner-login');
+});
+
+Route::get('logout', 'AuthController@getLogout');
+
+
